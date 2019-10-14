7946fa5be074b8dd10baefe0ad2d5de23a338b28
Juan Daniel Vargas Cordero <juandanielvargascordero@gmail.com>
2019-10-14 17:25:16 -0600

    Script to retrieve the file name with the latest date in its name

---
+++ getFileNameByLatestDate.sh
@@ -0,0 +1,27 @@
+let year=0
+let month=0
+let day=0
+let file
+while read -r filename; do
+  dateString=$(echo "${filename}" | grep -Po '\d{6}')
+  monthTmp=${dateString:0:2}
+  dayTmp=${dateString:2:2}
+  yearTmp=${dateString:4:2}
+  if [[ $year < $yearTmp ]]; then
+    year=$yearTmp
+    month=$monthTmp
+    day=$dayTmp
+    file=$filename
+  elif [[ $year == $yearTmp && $month < $monthTmp ]]; then
+    year=$yearTmp
+    month=$monthTmp
+    day=$dayTmp
+    file=$filename
+  elif [[ $year == $yearTmp && $month == $monthTmp && $day < $dayTmp ]]; then
+    year=$yearTmp
+    month=$monthTmp
+    day=$dayTmp
+    file=$filename
+  fi
+done < <(grep -ircl --exclude="test.sh" $1 .)
+echo $file
